940abd82f00d72afa8e9a3b073d45fd56870f304
saadkhan6670IX <asad.khan@integrationxperts.com>
2022-09-17 12:30:49 +0500

    env added for github private key

--- api/searchApi.ts
+++ api/searchApi.ts
@@ -1,12 +1,12 @@
-import axios from "axios";
 import ISearchRequest from "../models/requests/ISearchRequest.interface";
 import DataInterface from "../models/DataInterface.interface";
+import { axiosInstance } from ".";
 
 export default {
   async search(body: ISearchRequest): Promise<DataInterface> {
     try {
-      const response = await axios.get(
-        `https://api.github.com/search/${body.searchType}?q=${body.searchText}&page=${body.page}`
+      const response = await axiosInstance.get(
+        `/search/${body.searchType}?q=${body.searchText}&page=${body.page}`
       );
       return response.data as DataInterface;
     } catch (error) {
